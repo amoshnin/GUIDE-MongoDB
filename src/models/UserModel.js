1b64b36d02f10e9c77148ad4f2f6e7bae37898e2
Artem Moshnin <tap.kap.tap@gmail.com>
2020-12-07 17:28:03 +0100

    Validation done

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -3,6 +3,10 @@ const { model, Schema } = require("mongoose")
 const UserSchema = new Schema({
   name: {
     type: String,
+    validate: {
+      validator: (text) => text.length > 2,
+      message: "Name must be longer than 2 characters",
+    },
     required: [true, "Name is required"],
   },
   postCount: Number,
--- test/validation_test.js
+++ test/validation_test.js
@@ -3,15 +3,31 @@ const UserModel = require("../src/models/UserModel")
 
 // -----------------------------------------------------------------------------------
 
-// -----------------------------------------------------------------------------------
-
 describe("Validating records", () => {
-  it("Validate => requires a user name", () => {
+  it("Validate => requires a name", () => {
     const user = new UserModel({ name: undefined })
     const validationResult = user.validateSync()
     const { message } = validationResult.errors.name
-    console.log(message)
 
     assert(message === "Name is required")
+  })
+
+  it("Validate => requires a name.length > 2", () => {
+    const user = new UserModel({ name: "A" })
+    const validationResult = user.validateSync()
+    const { message } = validationResult.errors.name
+
+    assert(message === "Name must be longer than 2 characters")
+  })
+
+  it("Validate => Disallows invalid records from being saved", async () => {
+    const user = new UserModel({ name: "Al" })
+
+    try {
+      await user.save()
+    } catch (error) {
+      const { message } = error.errors.name
+      assert(message === "Name must be longer than 2 characters")
+    }
   })
 })
